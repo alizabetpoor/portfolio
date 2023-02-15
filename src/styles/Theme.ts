1e792827369fa69f23c2224457c16d0f8ccb03c8
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2023-02-15 22:14:21 +0330

    feat/config_base_settings add sass package add sfmono and calibre fonts add default mixins for media queries setting add default variables of the project as theme setup global css settings

---
+++ src/styles/Theme.ts
@@ -0,0 +1,12 @@
+const Theme = {
+    yellow:"#f1c40f",
+    lightGray:"#b6b5ab",
+    gray:"#a3a3a3",
+    darkGray:"#545450",
+    white:"#cdcdcd",
+    lightBlack:"#484848",
+    black:"#242424",
+    hoverGray:"#ebeae7",
+}
+
+export default Theme
