52634d0c0b178e7bc72e5194ad9ca152ff444509
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2023-02-17 19:29:01 +0330

    feat/about_section add aboutSection component add aboutSection data add personal image

---
+++ src/components/AboutSection/index.tsx
@@ -0,0 +1,36 @@
+import Box from "@/core/Box";
+import Label from "@/core/Label";
+import Title from "../Title";
+import { aboutSectionData } from "@/static/components/aboutSection/aboutSectionData";
+import Image from "next/image";
+import styles from "./style.module.scss";
+
+const AboutSection = () => {
+  return (
+    <div className={styles.aboutSection}>
+      <Title id="about" number={1} text="About Me" />
+      <div className={styles.aboutSection__describe}>
+        <Box className={styles.aboutSection__describe__box}>
+          {aboutSectionData.describeText}
+          <div className={styles.aboutSection__describe__box__skills}>
+            {aboutSectionData.skills.map((skill) => (
+              <Label key={skill.id} text={skill.name} />
+            ))}
+          </div>
+        </Box>
+        <div className={styles.aboutSection__describe__photo}>
+          <div className={styles.aboutSection__describe__photo__container}>
+            <Image
+              alt="alizabetpour | علی ضابط پور"
+              src="/assets/images/personalpic.jpg"
+              layout="fill"
+              className={styles.aboutSection__describe__photo__container__image}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
---
+++ src/static/components/aboutSection/aboutSectionData.ts
@@ -0,0 +1,11 @@
+export const aboutSectionData = {
+  describeText: `Hello, I'm a front-end developer with over a year of experience working with JavaScript and the React library. I'm currently studying for a bachelor's degree in computer engineering, which has given me a strong foundation in computer science and programming principles. I'm passionate about creating user-friendly, responsive web interfaces that enhance the user experience. I'm proficient in HTML, CSS, JavaScript, and React, and I'm always looking to expand my skill set and stay up-to-date with the latest developments in front-end web development.`,
+  skills: [
+    { id: 1, name: "JavaScript" },
+    { id: 2, name: "Next.js" },
+    { id: 3, name: "Typescript" },
+    { id: 4, name: "React" },
+    { id: 5, name: "Sass" },
+    { id: 6, name: "Git" },
+  ],
+};
