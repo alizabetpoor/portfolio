208b6e5a1664ae0ff080fa7bcbd9609d25f4ed9b
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2023-02-25 10:30:22 +0330

    feat/v1.0 render all component in the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
+import AboutSection from "@/components/AboutSection";
+import BioSection from "@/components/BioSection";
+import ContactSection from "@/components/ContactSection";
+import Email from "@/components/Email";
+import GoUp from "@/components/GoUp";
+import Social from "@/components/Social";
+import WorkExperienceSection from "@/components/WorkExperienceSection";
 
 export default function Home() {
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <BioSection />
+      <AboutSection />
+      <WorkExperienceSection />
+      <ContactSection />
+      <GoUp />
+      <Email />
+      <Social />
+    </div>
+  );
 }
